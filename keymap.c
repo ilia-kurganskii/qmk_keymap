a46a5d77bb330a19f7acaeebe19bb2c050424b01
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2025-03-27 00:38:45 +0100

    fix: fix c code

--- keymap.c
+++ keymap.c
@@ -3,7 +3,6 @@
 #include QMK_KEYBOARD_H
 
 enum sofle_layers {
-    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
     _COLEMAKDH,
     _LOWER,
@@ -14,9 +13,6 @@ enum sofle_layers {
 enum custom_keycodes {
     KC_QWERTY = SAFE_RANGE,
     KC_COLEMAKDH,
-    KC_LOWER,
-    KC_RAISE,
-    KC_ADJUST,
     KC_D_MUTE,
     KC_PRVWD,
     KC_NXTWD,
@@ -46,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_D_MUTE,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTL, KC_LOWER, KC_ENT,      KC_SPC,  TL_UPPR, KC_RAISE, KC_RALT, KC_RGUI
+                 KC_LGUI,KC_LALT,TL_LOWR, KC_SPC, KC_TAB,      KC_ENT,  KC_BSPC, TL_UPPR, KC_RALT, KC_RGUI
 ),
 
 
@@ -131,31 +127,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_COLEMAKDH:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_COLEMAKDH);
-            }
-            return false;
-        case KC_LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case KC_RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case KC_ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
             }
             return false;
         case KC_D_MUTE:
