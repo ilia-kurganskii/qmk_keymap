5ff32228aecc507c8e79ce235641bd6c83d1aa45
Ilia Kurganskii <ilia.kurganskii@bitvavo.com>
2025-08-18 19:37:11 +0200

    fix: fix build issue

--- keymap.c
+++ keymap.c
@@ -55,18 +55,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                        KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_GRV,
     KC_ESC,    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
     KC_TAB,    HOME_A,     HOME_S,     HOME_D,     HOME_F,     KC_G,                        KC_H,       HOME_J,     HOME_K,     HOME_L,     HOME_SCLN,  KC_QUOT,
-    KC_LSFT,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                        KC_MUTE,    KC_D_MUTE,  KC_N,       KC_M,       KC_COMM,    KC_DOT,    KC_SLSH,    KC_RSFT,
-    KC_LGUI,   KC_LALT,    TL_LOWR,    KC_SPC,     KC_TAB,                                              KC_ENT,     KC_BSPC,    TL_UPPR,    KC_RALT,    KC_RGUI
+    KC_LSFT,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,    KC_MUTE, KC_D_MUTE, KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
+                           KC_LGUI,    KC_LALT,    TL_LOWR,    KC_SPC,  KC_TAB,     KC_ENT, KC_BSPC,    TL_UPPR,    KC_RALT,    KC_RGUI
 ),
 
 
 
 [_COLEMAKDH] = LAYOUT(
-    _______,  KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                      KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
-    _______,  KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,                      KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    _______,
-    _______,  LCTL_T(KC_A), LALT_T(KC_R), LGUI_T(KC_S), LSFT_T(KC_T), KC_G,         KC_M,       RSFT_T(KC_N), RGUI_T(KC_E), RALT_T(KC_I), RCTL_T(KC_O), _______,
-    _______,  KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,                      _______,    _______,    KC_K,       KC_H,       KC_COMM,    KC_DOT,    KC_SLSH,    _______,
-    _______,  _______,    _______,    _______,    _______,    _______,                  _______,    _______,    _______,    _______,    _______
+    _______,  KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                            KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
+    _______,  KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,                            KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    _______,
+    _______,  KC_A,       KC_R,       KC_S,       KC_T,       KC_G,                            KC_M,       KC_N,       KC_E,       KC_I        KC_O,       _______,
+    _______,  KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,     _______,   _______,    KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_SLSH,    _______,
+                          _______,    _______,    _______,    _______,  _______,   _______,    _______,    _______,    _______,    _______
 ),
 
 /* LOWER
