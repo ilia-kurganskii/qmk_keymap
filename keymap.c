b6e1ed245a5b7265f17b2e189eb009210d560bd1
Ilia Kurganskii <ilia.kurganskii@bitvavo.com>
2025-08-24 23:49:03 +0200

    fix

--- keymap.c
+++ keymap.c
@@ -101,10 +101,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
-[_SYM] = LAYOUT(
+[_SYM] = LAYOUT(4
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   _______,
   _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______,   _______,
-  _______, KC_1, LCTL_T(KC_2), LALT_T(KC_3), LGUI_T(KC_4), LSFT_T(KC_5), RSFT_T(KC_6), RGUI_T(KC_7), LALT_T(KC_8), RCTL_T(KC_9), KC_0, _______,
+  _______, LCTL_T(KC_1), LALT_T(KC_2), LGUI_T(KC_3), LSFT_T(KC_4), KC_5, KC_6, RSFT_T(KC_7), RGUI_T(KC_8), LALT_T(KC_9), RCTL_T(KC_0), _______,
   _______, KC_SLSH, KC_GRV, KC_MINS, _______, KC_F11, _______,       _______, KC_F12,  _______, KC_LBRC, KC_RBRC, _______, _______,
                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
